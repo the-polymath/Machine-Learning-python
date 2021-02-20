013c702bd513f201d617d0e910306b524baf90a7
Raj G <rajgandhi784@gmail.com>
2021-02-20 15:26:36 +0530

    neural

--- Neural network/neural1.py
+++ Neural network/neural1.py
@@ -13,5 +13,29 @@ class_names = ['T-shirts/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
 train_img = train_img / 255.0
 test_img = test_img / 255.0
 
+'''
 plt.imshow(train_img[7], cmap=plt.cm.binary)
 plt.show()
+'''
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", metrics=["accuracy"], loss="sparse_categorical_crossentropy")
+
+model.fit(train_img, train_labels, epochs=8)
+
+test_loss, test_acc = model.evaluate(test_img, test_labels)
+print(test_loss, test_acc)
+
+model_predict = model.predict(test_img)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_img[i], cmap=plt.cm.binary)
+    plt.xlabel("actual" + class_names[i])
+    plt.title("Prediction" + class_names[np.argmax(model_predict[i])])
+    plt.show()
